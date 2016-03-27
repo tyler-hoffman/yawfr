67acf9608033525a39a132d525eaba31cf4abaec
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2016-03-26 20:45:05 -0400

    Indent like a reasonable person

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,32 +11,32 @@ define([
 
           gl.useProgram(program);
 
-    // look up where the vertex data needs to go.
-    var positionLocation = gl.getAttribLocation(program, 'a_position');
-    var xMin = -1.0,
-        xMax = 1.0,
-        yMin = -1.0,
-        yMax = 1.0;
+      // look up where the vertex data needs to go.
+      var positionLocation = gl.getAttribLocation(program, 'a_position');
+      var xMin = -1.0,
+          xMax = 1.0,
+          yMin = -1.0,
+          yMax = 1.0;
 
-    // Create a buffer and put a single clipspace rectangle in
-    // it (2 triangles)
-    var buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array([
-            xMin, yMin,
-            xMax, yMin,
-            xMin, yMax,
-            xMin, yMax,
-            xMax, yMin,
-            xMax, yMax]),
-        gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(positionLocation);
-    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+      // Create a buffer and put a single clipspace rectangle in
+      // it (2 triangles)
+      var buffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+      gl.bufferData(
+          gl.ARRAY_BUFFER,
+          new Float32Array([
+              xMin, yMin,
+              xMax, yMin,
+              xMin, yMax,
+              xMin, yMax,
+              xMax, yMin,
+              xMax, yMax]),
+          gl.STATIC_DRAW);
+      gl.enableVertexAttribArray(positionLocation);
+      gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
 
-    // draw
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
+      // draw
+      gl.drawArrays(gl.TRIANGLES, 0, 6);
 
     };
 
